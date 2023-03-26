393221af1a45b163c3a6efbf0f6dd0c628ca3759
suhuq007@gmail.com <suhuq007@gmail.com>
2023-03-26 13:10:28 +0530

    caching the data for offline

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,19 @@
 self.addEventListener('install', e => {
-  // e.waitUntil(
-  //   cache.open('static')
-  //   .then(cache => {
-  //   })
-  // )
+  e.waitUntil(
+    caches.open('static')
+    .then(cache => {
+      return cache.addAll(['./', './icon-128x128.png', './icon-192x192.png'])
+    })
+  )
+});
+
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+    .then(res => {
+      return res || fetch(e.request)
+    })
+  )
+
 })
