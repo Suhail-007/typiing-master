337ccb5e4262bd68ff9bce0ff277cc6553ce621f
suhuq007@gmail.com <suhuq007@gmail.com>
2023-03-29 11:53:38 +0530

    added cra template of pwa

--- public/sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('static')
-    .then(cache => {
-      return cache.addAll(['./', './icon-128x128.png', './icon-192x192.png'])
-    })
-  )
-});
