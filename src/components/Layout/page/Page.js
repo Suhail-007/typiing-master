32cbad77887b60ad77f80bf3153ef70604444d11
Suhail-007 <mohdsuhail51998@gmail.com>
2023-06-04 12:37:36 +0530

    change two column layout for desktop

--- src/components/Layout/page/Page.js
+++ src/components/Layout/page/Page.js
@@ -1,3 +1,5 @@
+import { useLocation } from 'react-router-dom';
+
 import CorrectPara from './CorrectPara';
 import TypedPara from './TypedPara';
 
@@ -5,26 +7,31 @@ import styles from './page.module.scss';
 
 
 export default function Page({ changeWord, inputHandler, checkWord, sentence, className }) {
+  const { pathname } = useLocation();
 
   function onInputHandler(e) {
     inputHandler(e);
     checkWord();
   }
 
+ 
+  const generatedTextClassName = pathname === '/word' ? styles.words : styles.sentence;
+
   return (
     <main className={`${styles.main} ${className}`}>
-      <section className={styles['generated-text']}>
+      <section className={`${styles['generated-text']} ${generatedTextClassName}`}>
         <p>
-          {sentence()} 
+          {sentence()}
         </p>
       </section>
       <section className={styles['textarea-cont']}>
-        <textarea autoFocus onKeyDown={changeWord} onInput={onInputHandler} type='text' placeholder='Press space to get new word'></textarea>  
+        <textarea autoFocus onKeyDown={changeWord} onInput={onInputHandler} type='text' placeholder='Press space to get new word'></textarea>
       </section>
-      
-      <CorrectPara className={styles['correct-para']} />
-      
-      <TypedPara className={styles['typed-para']} />
-    </main>
+
+      <div className={styles['typed-correct-para-cont']}>
+        <CorrectPara className={styles['correct-para']} />
+        <TypedPara className={styles['typed-para']} />
+      </div>
+    </main >
   )
 }
